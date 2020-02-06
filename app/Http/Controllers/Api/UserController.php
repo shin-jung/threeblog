04cf38a60d76ecb5944aeb8af9ad8e7bdc4e382e
shin-jung <shiro210218@gmail.com>
2020-02-06 15:14:32 +0800

    move UserService

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Services\UserService;
+use App\Services\Api\UserService;
 use JWTAuth;
 
 class UserController extends Controller
--- app/Services/Api/UserService.php
+++ app/Services/Api/UserService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Api;
 
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
