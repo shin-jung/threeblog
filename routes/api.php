da3cc2950f021eaee120eec1bdfccb80b339cebd
ShinRungLee <shinrung.lee@language-center.com.tw>
2021-11-28 13:54:53 +0800

    fix:modify

--- routes/api.php
+++ routes/api.php
@@ -16,30 +16,28 @@ Route::post('/login', 'Api\AuthController@login');
 
 Route::post('/register', 'Api\AuthController@register');
 
-Route::group(['middleware' => 'auth.jwt'], function(){
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('/user', 'Api\UserController@index')->middleware('user');
 
-	Route::get('/user', 'Api\UserController@index')->middleware('user');
-
-	Route::post('/logout', 'Api\AuthController@logout');
+    Route::post('/logout', 'Api\AuthController@logout');
 });
 
-Route::group(['prefix' => 'article', 'middleware' => 'auth.jwt'], function(){
+Route::group(['prefix' => 'article', 'middleware' => 'auth.jwt'], function () {
+    Route::get('/home', 'Api\ArticleController@index');
 
-	Route::get('/home', 'Api\ArticleController@index');
+    Route::post('/store', 'Api\ArticleController@store');
 
-	Route::post('/store', 'Api\ArticleController@store');
+    Route::get('/show/{id?}', 'Api\ArticleController@show');
 
-	Route::get('/show/{id?}', 'Api\ArticleController@show');
+    Route::post('/update/{id?}', 'Api\ArticleController@update')->middleware('article');
 
-	Route::post('/update/{id?}', 'Api\ArticleController@update')->middleware('article');
-
-	Route::post('/delete/{id?}', 'Api\ArticleController@destory')->middleware('article');
+    Route::post('/delete/{id?}', 'Api\ArticleController@destory')->middleware('article');
 });
 
-Route::fallback(function() {
+Route::fallback(function () {
     return response()->json([
         'success' => false,
-    	'message' => 'Sorry, can not find this web.',
-    	'data' => '',
+        'message' => 'Sorry, can not find this web.',
+        'data' => '',
     ], 403);
 });
