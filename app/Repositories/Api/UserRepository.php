87d26a1abb083402b93f1474ea4dc1e6a7374420
ShinRungLee <shinrung.lee@language-center.com.tw>
2022-01-11 16:56:27 +0800

    feat:修改註冊功能
    why:
    以前寫code沒有詳細了解分類和request的用途
    how:
    將service和repository分類好，登入、註冊、登出雖然都在AuthController，但其實撈資料的方式都是到userService和userRepository取得。因此修改。

--- app/Repositories/Api/UserRepository.php
+++ app/Repositories/Api/UserRepository.php
@@ -2,23 +2,19 @@
 
 namespace App\Repositories\Api;
 
-use App\User;
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserRepository
 {
+    public function indexUser()
+    {
+        return User::all();
+    }
 
-	public function indexUser()
-	{
-		return User::all();
-	}
-
-	public function register(Request $request)
-	{
-		return User::create([
+    public function register($request)
+    {
+        return User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
-	}
-}
